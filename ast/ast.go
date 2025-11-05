638d7efe697f6dafc61cb93e55f5aa84c66d2d0e
Asher Samuel <ashersam116@gmail.com>
2025-11-05 15:05:03 +0300

    statments implement String

--- ast/ast.go
+++ ast/ast.go
@@ -1,9 +1,14 @@
 package ast
 
-import "github.com/devasherr/lambda/token"
+import (
+	"bytes"
+
+	"github.com/devasherr/lambda/token"
+)
 
 type Node interface {
 	TokenLiteral() string
+	String() string
 }
 
 type Statment interface {
@@ -28,6 +33,14 @@ func (p *Program) TokenLiteral() string {
 	return p.Statments[0].TokenLiteral()
 }
 
+func (p *Program) String() string {
+	var out bytes.Buffer
+	for _, stmt := range p.Statments {
+		out.WriteString(stmt.String())
+	}
+	return out.String()
+}
+
 type Identifier struct {
 	Token token.Token
 	Value string
@@ -35,6 +48,7 @@ type Identifier struct {
 
 func (i *Identifier) expressionNode()      {}
 func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+func (i *Identifier) String() string       { return i.Value }
 
 type LetStatement struct {
 	Token token.Token
@@ -44,6 +58,20 @@ type LetStatement struct {
 
 func (ls *LetStatement) statementNode()       {}
 func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+func (ls *LetStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(ls.TokenLiteral() + " ")
+	out.WriteString(ls.Name.String())
+	out.WriteString(" = ")
+
+	if ls.Value != nil {
+		out.WriteString(ls.Value.String())
+	}
+
+	out.WriteString(";")
+	return out.String()
+}
 
 type ReturnStatment struct {
 	Token       token.Token
@@ -52,3 +80,33 @@ type ReturnStatment struct {
 
 func (rs *ReturnStatment) statementNode()       {}
 func (rs *ReturnStatment) TokenLiteral() string { return rs.Token.Literal }
+func (rs *ReturnStatment) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(rs.TokenLiteral() + " ")
+
+	if rs.ReturnValue != nil {
+		out.WriteString(rs.ReturnValue.String())
+	}
+
+	out.WriteString(";")
+	return out.String()
+}
+
+// ExpressionStatment allow for one line expressions to be perceived as statments
+// let x = 10;
+// x += 10;
+// both lines are valid statements
+type ExpressionStatment struct {
+	Token      token.Token // first token of the expression
+	Expression Expression
+}
+
+func (es *ExpressionStatment) statementNode()       {}
+func (es *ExpressionStatment) TokenLiteral() string { return es.Token.Literal }
+func (es *ExpressionStatment) String() string {
+	if es.Expression != nil {
+		return es.Expression.String()
+	}
+	return ""
+}
