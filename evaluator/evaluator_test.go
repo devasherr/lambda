1698ee33fe986b663ad507fee8767a9524a53787
Asher Samuel <ashersam116@gmail.com>
2025-11-13 19:29:37 +0300

    string concatenate

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -207,6 +207,7 @@ func TestErrorHandling(t *testing.T) {
 		`,
 			"unknown operator: BOOLEAN + BOOLEAN",
 		},
+		{`"Hello" - "Hello"`, "unknown operator: STRING - STRING"},
 	}
 
 	for _, tt := range tests {
@@ -298,6 +299,20 @@ addTwo(2);`
 
 func TestStringLiteral(t *testing.T) {
 	input := `"Hello World!"`
+
+	evaluated := testEval(input)
+	str, ok := evaluated.(*object.String)
+	if !ok {
+		t.Fatalf("object is not String. got=%T (%+v)", evaluated, evaluated)
+	}
+
+	if str.Value != "Hello World!" {
+		t.Fatalf("String has wrong value. got=%q", str.Value)
+	}
+}
+
+func TestStringConcatenation(t *testing.T) {
+	input := `"Hello" + " " + "World!"`
 
 	evaluated := testEval(input)
 	str, ok := evaluated.(*object.String)
